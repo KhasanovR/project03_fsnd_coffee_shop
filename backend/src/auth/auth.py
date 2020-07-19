2d4ae5a6ec763f5674dbde1306165d211ef94870
Rakhmatjon Khasanov <rakhmatjon.khasanov2711@gmail.com>
2020-07-19 23:51:06 +0500

    Change AUTH0 config domain & audience

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,14 +1,18 @@
 import json
-from flask import request, _request_ctx_stack
+from flask import request
+from flask import _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'fsnd-khasanovr.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'Coffee'
 
-# AuthError Exception
+# ---------------------------------------------------------------------------- #
+# AuthError Exception                                                          #
+# ---------------------------------------------------------------------------- #
+
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
@@ -21,7 +25,9 @@ class AuthError(Exception):
         self.status_code = status_code
 
 
-# Auth Header
+# ---------------------------------------------------------------------------- #
+# Auth Headers                                                                 #
+# ---------------------------------------------------------------------------- #
 
 '''
 @TODO:(Done) Implement get_token_auth_header() method
